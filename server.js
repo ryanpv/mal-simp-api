35ceea85149a1872deff6805a9b2745910f23194
Ryan <ryanpvoong@gmail.com>
2023-02-14 12:57:06 -0500

    firebase functions

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const app = express();
 const dotenv = require("dotenv");
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
-const port = process.env.PORT;
+const functions = require('firebase-functions')
+const port = 6969
 
 
 app.use(cors({ 
   origin: [
     'http://localhost:3000',
-    'http://localhost:6969'
+    'http://localhost:6969',
+    'https://mal-simplified.web.app/'
   ],
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' }));
@@ -19,9 +21,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(require("./routes/routing.js"));
 
+
+
 app.listen(port, () => {
   console.log(`server connected to port ${port}`);
 });
 
+exports.api = functions.https.onRequest(app)
 
 // https://myanimelist.net/apiconfig/references/api/v2#operation/anime_get

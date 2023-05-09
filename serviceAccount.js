b42a916b33615fb363bd17fc4307554037a61578
Ryan <ryanpvoong@gmail.com>
2023-05-08 21:36:19 -0400

    attempting private key fix

--- serviceAccount.js
+++ serviceAccount.js
@@ -2,7 +2,7 @@ module.exports = {
   type: process.env.FB_SERVICE_ACCOUNT_TYPE,
   project_id: process.env.FB_SERVICE_ACCOUNT_PROJECT_ID,
   private_key_id: process.env.FB_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-  private_key: process.env.FB_SERVICE_ACCOUNT_PRIVATE_KEY,
+  private_key: process.env.FB_SERVICE_ACCOUNT_PRIVATE_KEY ? process.env.FB_SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
   client_email: process.env.FB_SERVICE_ACCOUNT_CLIENT_EMAIL,
   client_id: process.env.FB_SERVICE_ACCOUNT_CLIENT_ID,
   auth_uri: process.env.FB_SERVICE_ACCOUNT_AUTH_URI,

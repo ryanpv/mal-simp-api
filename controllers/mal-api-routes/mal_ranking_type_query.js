dcac12b6e32f81cf3180a1730cf80bd9b82f71bf
Ryan <ryanpvoong@gmail.com>
2023-08-17 20:50:40 -0400

    changed limit param for MAL api ranking query

--- controllers/mal-api-routes/mal_ranking_type_query.js
+++ controllers/mal-api-routes/mal_ranking_type_query.js
@@ -2,15 +2,16 @@ const axios = require('axios');
 
 const animeRankingQuery = async (req, res) => {
   // only returning 5 results for 'Top airing' page (used for homepage), all other pages return 8 results
-  const limit = req.params.rankType === 'airing' ? 5 : 8; 
+  // const limit = req.params.rankType === 'airing' ? 10 : 8; 
+  console.log('offset', req.params.offset);
   try {
     const getAnimeRanking = await 
-      axios.get(`https://api.myanimelist.net/v2/anime/ranking?ranking_type=${ req.params.rankType }&limit=${ limit }&offset=${ req.params.offset }&fields=pictures,status,mean,synopsis,num_episodes`, {
+      axios.get(`https://api.myanimelist.net/v2/anime/ranking?ranking_type=${ req.params.rankType }&limit=10&offset=${ req.params.offset }&fields=pictures,status,mean,synopsis,num_episodes`, {
         headers: {
           'X-MAL-CLIENT-ID': process.env.MAL_CLIENT_ID
         }
       })
-      // console.log('module data: ', getAnimeRanking.data);
+      // console.log('ranking data: ', getAnimeRanking.data.data[0].node.title);
     res.send(getAnimeRanking.data);
   } catch (err) {
     res.status(400).send(err)

ec5b381ec14e6e404d35bd33e0ea003cf09e04b0
Ryan <ryanpvoong@gmail.com>
2023-08-17 22:03:45 -0400

    fixed if statement for ENV

--- controllers/mal-auth/mal_code_challenge.js
+++ controllers/mal-auth/mal_code_challenge.js
@@ -1,5 +1,5 @@
 const malCodeChallenge = async (req, res) => {
-  if(process.env.NODE_ENV === 'development') {
+  if(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev') {
 // MUST BE REDIRECTED OTHERWISE MAL API CANNOT VERIFY CODE_CHALLENGE FOR WHATEVER REASON*****************
     await res.redirect('/callback');
   } else {
